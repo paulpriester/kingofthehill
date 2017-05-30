1cb2ff7a557723a82a3a1d7834ea7a50df0a94cc
paulpriester <paulpriester507@gmail.com>
2017-05-30 15:26:30 -0400

    update

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ for(x in myVideo ) {
 }
 
 }
+
+$(document).ready(function(){
+  		$(window).resize(function(){
+			    var footerHeight = $('.footer').outerHeight();
+			    var stickFooterPush = $('.push').height(footerHeight);
+		
+    			$('.wrapper').css({'marginBottom':'-' + footerHeight + 'px'});
+		    });
+		
+    		$(window).resize();
+	    });
